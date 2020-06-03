f760c19c851104b864f7bad2bbc3b9939e57eb5d
Eduardo Camacho <eduardocc97@hotmail.com>
2020-06-03 15:47:03 -0600

    Se anadio la BD y se hace la autenticacion para hacer el login con datos de la BD

--- CursoMVC/Controllers/AccessController.cs
+++ CursoMVC/Controllers/AccessController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CursoMVC.Models;
 
 namespace CursoMVC.Controllers
 {
@@ -17,9 +18,26 @@ namespace CursoMVC.Controllers
         {
             try
             {
+                using (corsomvcEntities db = new corsomvcEntities()) //inicializamos la BD
+                {
+                    //Hacemos una lista de los datos con una consulta a la BD, este metodo se llama LINQ
+                    var lst = from d in db.user
+                              where d.email == user && d.password == password && d.idState==1
+                              select d;
 
+                    if(lst.Count() > 0) //Verificamos que exista el usuario
+                    {
+                        user oUser = lst.First(); //Hacemos un tipo de dato llamado model.user
+                        Session["User"] = oUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario Invalido");
+                    }
+                }
 
-                return Content("1");
+        
             }
             catch (Exception ex)
             {
