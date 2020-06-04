101ed812296e9758cbffb52906e4d16dffaa1306
Eduardo Camacho <eduardocc97@hotmail.com>
2020-06-04 17:14:06 -0600

    Se anadio funcion al boton de eliminar

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -60,5 +60,46 @@ namespace CursoMVC.Controllers
             //Enviamos al usuario a la lista de usuarios
             return Redirect(Url.Content("~/User"));
         }
+        
+        public ActionResult Edit(int Id)
+        {
+            EditUserViewModel model = new EditUserViewModel();
+
+            using (var db = new corsomvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                model.Edad = (int)oUser.edad;
+                model.Email = oUser.email;
+                model.Id = oUser.id;
+            }
+                return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditUserViewModel model)
+        {
+            //Validar
+            if (!ModelState.IsValid) //Si hay un error en los datos que se envian al UserViewModel.cs
+            {
+                return View(model); //Regresa los datos ingresados (No se borran los campos que se han llenado)
+            }
+            using (var db = new corsomvcEntities())
+            {
+                var oUser = db.user.Find(model.Id);
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+
+                if(model.Password!=null && model.Password.Trim() != "") //Esta validacion debe estar en este orden, si no va a crashear
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified; //Con esto le decimos a la bd que va a ejecutar una edicion
+                db.SaveChanges(); //Guardamos los cambios
+
+            }
+
+            return Redirect(Url.Content("~/User"));
+        }
     }
 }
--- CursoMVC/Models/ViewModels/UserViewModel.cs
+++ CursoMVC/Models/ViewModels/UserViewModel.cs
@@ -27,4 +27,28 @@ namespace CursoMVC.Models.ViewModels
         [Required]
         public int Edad { get; set; }
     }
+
+    public class EditUserViewModel
+    {
+        public int Id { get; set; } //Capturamos el ID
+
+        [Required]
+        [EmailAddress] //Verificar que sea correo
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 1)] //Requerimos que se tenga como minimo un caracter y maximo 100 para validar
+        [Display(Name = "Correo Electrónico")] //Lo que queremos que se muestre en el label
+        public string Email { get; set; }
+
+        //Aqui la contraseña no es requerida forzosamente
+        [DataType(DataType.Password)] //Para que el input sea tipo password
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar contraseña")]
+        [DataType(DataType.Password)] //Para que el input sea tipo password
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")] //Le decimos que compare si la variable Password y la que sigue (ConfirmPassword) son iguales, es como un if
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
 }
